5afbb985f64f180424758bb978ff1ec4b980e09b
Favio Sauto <favio.sauto@unosquare.com>
2023-06-06 11:14:07 -0400

    Controllers, swagger and docker completed

--- database/local-data-seed/V1.1000__local_data_seed.sql
+++ database/local-data-seed/V1.1000__local_data_seed.sql
@@ -1,23 +1,6 @@
-INSERT INTO user ( username, email, password, admin) VALUES
-( 'faviosauto', 'faviosauto@gmail.com', 'password', TRUE );
+INSERT INTO public.users (username, email, password, admin) VALUES ('faviosauto', 'faviosauto@gmail.com', 'password', true);
 
-INSERT INTO team (short_name, localCity, country, stadium_id) VALUES
-( 'Athletic Club', 'Vizcaya', 'Spain', 1 ),
-( 'Atlético de Madrid', 'Madrid', 'Spain', 2 ),
-( 'Barcelona', 'Barcelona', 'Spain', 3 ),
-( 'Girona', 'Gerona', 'Spain', 4 ),
-( 'Granada', 'Granada', 'Spain', 5 ),
-( 'Las Palmas', 'Las Palmas de Gran Canaria', 'Spain', 6 ),
-( 'Mallorca', 'Islas baleares', 'Spain', 7 ),
-( 'Osasuna', 'Navarra', 'Spain', 8 ),
-( 'Rayo Vallecano', 'Madrid', 'Spain', 9 ),
-( 'Real Betis', 'Sevilla', 'Spain', 10 ),
-( 'Real Madrid', 'Madrid', 'Spain', 11 ),
-( 'Real Sociedad', 'Guipúzcua', 'Spain', 12 ),
-( 'Sevilla', 'Sevilla', 'Spain', 13 ),
-( 'Villareal', 'Castellón', 'Spain', 14 );
-
-INSERT INTO stadium (name, city, country) VALUES
+INSERT INTO public.stadiums (name, city, country) VALUES
 ( 'San Mamés', 'Vizcaya', 'Spain' ),
 ( 'Cívitas Metropolitano', 'Madrid', 'Spain' ),
 ( 'Spotify Camp Nou', 'Barcelona', 'Spain' ),
@@ -33,16 +16,31 @@ INSERT INTO stadium (name, city, country) VALUES
 ( 'Ramón Sánchez-Pizjuán', 'Sevilla', 'Spain' ),
 ( 'Estadio de la Cerámica', 'Castellón', 'Spain' );
 
-INSERT INTO competition (name, scope) VALUES
+INSERT INTO public.teams (name, city, stadium_id) VALUES
+( 'Athletic Club', 'Vizcaya', 1 ),
+( 'Atlético de Madrid', 'Madrid', 2 ),
+( 'Barcelona', 'Barcelona', 3 ),
+( 'Girona', 'Gerona', 4 ),
+( 'Granada', 'Granada', 5 ),
+( 'Las Palmas', 'Las Palmas de Gran Canaria', 6 ),
+( 'Mallorca', 'Islas baleares', 7 ),
+( 'Osasuna', 'Navarra', 8 ),
+( 'Rayo Vallecano', 'Madrid', 9 ),
+( 'Real Betis', 'Sevilla', 10 ),
+( 'Real Madrid', 'Madrid', 11 ),
+( 'Real Sociedad', 'Guipúzcua', 12 ),
+( 'Sevilla', 'Sevilla', 13 ),
+( 'Villareal', 'Castellón', 14);
+
+INSERT INTO public.competitions (name, scope) VALUES
 ( 'LaLiga Santander', 'Spain' ),
 ( 'UEFA Champions League', 'Europe' );
 
-INSERT INTO prediction (user_id, away_goals, home_goals) VALUES
+INSERT INTO public.predictions (user_id, away_goals, home_goals) VALUES
 ( 1, 2, 1 );
 
-INSERT INTO match (competition_id, stadium_id, away_team_id, home_team_id) VALUES
+INSERT INTO public.matches (competition_id, stadium_id, away_team_id, home_team_id) VALUES
 ( 1, 11, 1, 11 );
 
-INSERT INTO match_predictions (match_id, prediction_id) VALUES
+INSERT INTO public.match_predictions (match_id, prediction_id) VALUES
 ( 1, 1 );
-
--- database/migrations/v1.0__initial_schema.sql
+++ database/migrations/v1.0__initial_schema.sql
@@ -1,72 +1,75 @@
-CREATE TABLE user (
-    id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS public.users (
+    id SERIAL CONSTRAINT users_pk PRIMARY KEY,
     username TEXT,
     admin BOOLEAN,
     email TEXT,
     password TEXT
 );
 
-CREATE TABLE prediction (
-    id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS public.predictions (
+    id SERIAL CONSTRAINT prediction_pk PRIMARY KEY,
     away_goals INT,
     home_goals INT,
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE review (
-    id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS public.reviews (
+    id SERIAL CONSTRAINT reviews_pk PRIMARY KEY,
     title TEXT,
     content TEXT,
     user_id INT,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE competition (
-    id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS public.competitions (
+    id SERIAL CONSTRAINT competitions_pk PRIMARY KEY,
     name TEXT,
     scope TEXT
-)
-
-CREATE TABLE match (
-    id INT PRIMARY KEY,
-    stadium_id INT,
-    away_team_id INT,
-    home_team_id INT,
-    competition_id INT,
-    FOREIGN KEY (competition_id) REFERENCES competition(id)
-    FOREIGN KEY (stadium_id) REFERENCES stadium(id),
-    FOREIGN KEY (away_team_id) REFERENCES team(id),
-    FOREIGN KEY (home_team_id) REFERENCES team(id)
 );
 
-CREATE TABLE team (
-    id INT PRIMARY KEY,
-    short_name TEXT,
-    localCity TEXT,
-    stadium_id INT,
-    FOREIGN KEY (stadium_id) REFERENCES stadium(id)
-);
-
-CREATE TABLE stadium (
-    id INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS public.stadiums (
+    id SERIAL CONSTRAINT stadiums_pk PRIMARY KEY,
     name TEXT,
     city TEXT,
     country TEXT
 );
 
-CREATE TABLE match_reviews (
-    id INT PRIMARY KEY,
-    match_id INT,
-    review_id INT,
-    FOREIGN KEY (match_id) REFERENCES  match(id),
-    FOREIGN KEY (review_id) REFERENCES review(id)
+CREATE TABLE IF NOT EXISTS public.teams (
+    id SERIAL CONSTRAINT teams_pk PRIMARY KEY,
+    name TEXT,
+    city TEXT,
+    stadium_id INT,
+    FOREIGN KEY (stadium_id) REFERENCES stadiums(id)
 );
 
-CREATE TABLE match_predictions (
-    id INT PRIMARY KEY,
+
+CREATE TABLE IF NOT EXISTS public.matches (
+    id SERIAL CONSTRAINT matches_pk PRIMARY KEY,
+    stadium_id INT,
+    away_team_id INT,
+    home_team_id INT,
+    competition_id INT,
+    FOREIGN KEY (competition_id) REFERENCES competitions(id),
+    FOREIGN KEY (stadium_id) REFERENCES stadiums(id),
+    FOREIGN KEY (away_team_id) REFERENCES teams(id),
+    FOREIGN KEY (home_team_id) REFERENCES teams(id)
+);
+
+CREATE TABLE IF NOT EXISTS public.match_reviews (
+    id SERIAL CONSTRAINT match_reviews_pk PRIMARY KEY,
+    match_id INT,
+    review_id INT,
+    FOREIGN KEY (match_id) REFERENCES  matches(id),
+    FOREIGN KEY (review_id) REFERENCES reviews(id)
+);
+
+CREATE TABLE IF NOT EXISTS public.match_predictions (
+    id SERIAL CONSTRAINT match_predictions_pk PRIMARY KEY,
     match_id INT,
     prediction_id INT,
-    FOREIGN KEY (match_id) REFERENCES match(id),
-    FOREIGN KEY (prediction_id) REFERENCES prediction(id)
+    created_date TIMESTAMP DEFAULT NOW() NOT NULL,
+    updated_date TIMESTAMP,
+    FOREIGN KEY (match_id) REFERENCES matches(id),
+    FOREIGN KEY (prediction_id) REFERENCES predictions(id)
 );
